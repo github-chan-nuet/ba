a16a119aa28d244d016d15bb7a96e9043fcb0a15
Patrick Scheidegger <p.scheidegger1@sunrise.ch>
2025-03-25 12:33:15 +0100

    init backend

---
+++ PhishingOrchestrator/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	sMux := http.NewServeMux()
+	sMux.HandleFunc("GET /api/health", getHealth)
+	addr, ok := os.LookupEnv("ORCH_WEBSERVER_ADDR")
+	if !ok {
+		addr = "localhost:8080"
+	}
+	slog.Info("Web server listening...", "address", addr)
+	err := http.ListenAndServe(addr, sMux)
+	slog.Error("Web server stopped", "error", err)
+	os.Exit(1)
+}
+
+func getHealth(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
---
+++ PhishingOrchestrator/readme.md
@@ -0,0 +1,4 @@
+# Structure
+Guidelines:
+- [Official Guidelines](https://go.dev/doc/modules/layout)
+- [Unofficial best practices](https://github.com/golang-standards/project-layout)
