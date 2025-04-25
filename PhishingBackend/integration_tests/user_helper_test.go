10f4dddd11e1ce4686b2f80405f128746bf007e9
Patrick.Scheidegger <patrick.scheidegger@ost.ch>
2025-04-25 14:25:08 +0200

    create integration tests and helper functionality

--- PhishingBackend/integration_tests/user_helper_test.go
+++ PhishingBackend/integration_tests/user_helper_test.go
@@ -5,7 +5,11 @@ package integration_tests
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"phishing_backend/internal/infrastructure/presentation/api"
 	"strings"
@@ -35,4 +39,27 @@ func createUser(t *testing.T) *api.UserPostModel {
 		t.FailNow()
 	}
 	return &reqBody
+}
+
+func getJwtTokenForUser(t *testing.T, user *api.UserPostModel) string {
+	reqBody := api.UserAuthentication{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+	marshal, _ := json.Marshal(reqBody)
+	req, err := http.NewRequest(http.MethodPost, ts.URL+"/api/users/login", bytes.NewReader(marshal))
+	assert.NoError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	binToken, _ := io.ReadAll(resp.Body)
+	return string(binToken)
+}
+
+func getUserId(jwtToken string) uuid.UUID {
+	claimString := strings.Split(jwtToken, ".")[1]
+	decoded, _ := base64.StdEncoding.DecodeString(claimString)
+	var result map[string]interface{}
+	json.Unmarshal(decoded, &result)
+	return uuid.MustParse(result["id"].(string))
 }
