c0c2428282bd3c0dc4808e508ae55107f2323b0d
Patrick Scheidegger <p.scheidegger1@sunrise.ch>
2025-04-25 16:04:44 +0200

    create integration test

---
+++ PhishingBackend/integration_tests/get_user_test.go
@@ -0,0 +1,36 @@
+//go:build integration
+
+package integration_tests
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"phishing_backend/internal/infrastructure/presentation/api"
+	"testing"
+)
+
+func TestUserCanGet(t *testing.T) {
+	// given
+	user := createUser(t)
+	jwtToken := getJwtTokenForUser(t, user)
+	userId := getUserId(jwtToken)
+
+	req, _ := http.NewRequest(http.MethodGet, ts.URL+"/api/users/"+userId.String(), nil)
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+
+	// when
+	resp, err := http.DefaultClient.Do(req)
+
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	var gotUser api.User
+	err = json.NewDecoder(resp.Body).Decode(&gotUser)
+	assert.NoError(t, err)
+	assert.Equal(t, user.Firstname, *gotUser.Firstname)
+	assert.Equal(t, user.Lastname, *gotUser.Lastname)
+	assert.Equal(t, user.Email, *gotUser.Email)
+	assert.Equal(t, 0, *gotUser.Level)
+	assert.Equal(t, 0, *gotUser.TotalExperience)
+}
