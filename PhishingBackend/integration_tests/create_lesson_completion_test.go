5b48872abe7a7b245d98a9803e7194cd6019a076
Patrick.Scheidegger <patrick.scheidegger@ost.ch>
2025-04-27 16:33:38 +0200

    add integration tests

--- PhishingBackend/integration_tests/create_lesson_completion_test.go
+++ PhishingBackend/integration_tests/create_lesson_completion_test.go
@@ -36,3 +36,29 @@ func TestLessonCanBeCompleted(t *testing.T) {
 	assert.NotNil(t, expGain.NewLevel)
 	assert.Equal(t, int64(2), *expGain.NewLevel)
 }
+
+func TestTheSameLessonCantBeCompletedTwice(t *testing.T) {
+	t.SkipNow() // todo enable this test once the same lesson can't be inserted twice
+
+	// given
+	user := createUser(t)
+	jwtToken := getJwtTokenForUser(t, user)
+
+	reqBody := api.Lesson{LessonId: uuid.New()}
+	marshal, _ := json.Marshal(reqBody)
+	url := ts.URL + "/api/courses/" + uuid.NewString() + "/completions"
+	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewReader(marshal))
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	// and lesson was already completed
+	http.DefaultClient.Do(req)
+	// and create new request for the same lesson completion
+	req2, _ := http.NewRequest(http.MethodPost, url, bytes.NewReader(marshal))
+	req2.Header.Set("Authorization", "Bearer "+jwtToken)
+
+	// when
+	resp, err := http.DefaultClient.Do(req2)
+
+	// then
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+}
