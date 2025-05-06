37c9d988c0552d62e9ad187b75c6701434cfaf85
Patrick Scheidegger <p.scheidegger1@sunrise.ch>
2025-05-06 16:30:57 +0200

    fix validation, add integration test

--- PhishingBackend/integration_tests/create_user_test.go
+++ PhishingBackend/integration_tests/create_user_test.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"phishing_backend/internal/adapters/presentation/api"
 	"phishing_backend/internal/domain_services/services"
@@ -38,4 +39,36 @@ func TestNewUserCanBeCreated(t *testing.T) {
 	assert.Equal(t, reqBody.Lastname, user.Lastname)
 	expectedPw := services.HashPassword(reqBody.Password)
 	assert.Equal(t, expectedPw, user.Password)
+}
+
+func TestNewUserCantHaveSameEmail(t *testing.T) {
+	// given
+	reqBody := api.UserPostModel{
+		Email:     createRandomEmail(),
+		Password:  "password",
+		Firstname: "John",
+		Lastname:  "Doe",
+	}
+	marshal, _ := json.Marshal(reqBody)
+	// a user exists
+	req, _ := http.NewRequest(http.MethodPost, ts.URL+"/api/users", bytes.NewReader(marshal))
+	_, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	req2, _ := http.NewRequest(http.MethodPost, ts.URL+"/api/users", bytes.NewReader(marshal))
+
+	// when
+	resp, err := http.DefaultClient.Do(req2)
+
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusUnprocessableEntity, resp.StatusCode)
+	var gotProb api.ProblemDetail
+	err = json.NewDecoder(resp.Body).Decode(&gotProb)
+	assert.NoError(t, err)
+	want := api.ProblemDetail{
+		Type:   createErrorDetailUrn("email-already-used"),
+		Title:  "Diese Email wird bereits verwendet",
+		Status: 422,
+	}
+	assert.Equal(t, want, gotProb)
 }
