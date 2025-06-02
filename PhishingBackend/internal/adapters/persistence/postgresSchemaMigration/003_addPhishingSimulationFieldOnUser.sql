ffc056de4ca51079720ef3fe28ccca0374cede55
Patrick Scheidegger <p.scheidegger1@sunrise.ch>
2025-06-02 11:16:10 +0200

    fix merge conflicts

--- PhishingBackend/internal/adapters/persistence/postgresSchemaMigration/003_addPhishingSimulationFieldOnUser.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users ADD COLUMN participates_in_phishing_simulation boolean NULL;
-
-UPDATE users SET participates_in_phishing_simulation = false;
-
-ALTER TABLE users ALTER COLUMN participates_in_phishing_simulation SET NOT NULL;
