e354db949151d61f4867c6d2608ba028c82413f6
Mischa Binder <mischa.binder@stafag.ch>
2025-06-03 14:28:30 +0200

    Start Implementing Run Generation

--- PhishingBackend/internal/domain_services/services/phishing_run_service.go
+++ PhishingBackend/internal/domain_services/services/phishing_run_service.go
@@ -3,6 +3,9 @@ package services
 import (
 	"phishing_backend/internal/domain_model"
 	"phishing_backend/internal/domain_services/interfaces/email"
+	"phishing_backend/internal/domain_services/interfaces/repositories"
+
+	"github.com/google/uuid"
 )
 
 var _ PhishingRunService = (*PhishingRunServiceImpl)(nil)
@@ -12,10 +15,37 @@ type PhishingRunService interface {
 }
 
 type PhishingRunServiceImpl struct {
-	EmailSender email.EmailSender
+	EmailSender                  email.EmailSender
+	PhishingSimulationRepository repositories.PhishingSimulationRepository
 }
 
-func (s *PhishingRunServiceImpl) GenerateRun(*domain_model.User) error {
+func (s *PhishingRunServiceImpl) GenerateRun(user *domain_model.User) error {
+	templates, err := s.PhishingSimulationRepository.GetTemplates()
+	if err != nil {
+		return err
+	}
+	if len(templates) > 0 {
+		first := templates[0]
 
+		run := domain_model.PhishingSimulationRun{
+			ID:         uuid.New(),
+			UserFk:     user.ID,
+			TemplateFk: first.ID,
+		}
+		err = s.PhishingSimulationRepository.Create(&run)
+		if err != nil {
+			return err
+		}
+
+		email := domain_model.Email{
+			Content:   first.Content,
+			Recipient: user.Email,
+			Subject:   first.Subject,
+		}
+		err = s.EmailSender.Send(&email)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
