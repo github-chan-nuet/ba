426ba22d09dfb60237851d33c8dfac8ba51cf636
Mischa Binder <mischa.binder@stafag.ch>
2025-06-04 13:01:59 +0200

    Implement Logic to find most suitable template

--- PhishingBackend/internal/domain_model/phishing_simulation_content_template.go
+++ PhishingBackend/internal/domain_model/phishing_simulation_content_template.go
@@ -1,6 +1,13 @@
 package domain_model
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"log/slog"
+	"phishing_backend/internal/utils"
+	"regexp"
+
+	"github.com/google/uuid"
+)
 
 type PhishingSimulationContentTemplate struct {
 	ID                uuid.UUID `gorm:"type:uuid;primary_key;"`
@@ -8,4 +15,107 @@ type PhishingSimulationContentTemplate struct {
 	Content           string
 	ContentCategoryFk uuid.UUID
 	ContentCategory   *PhishingSimulationContentCategory `gorm:"foreignKey:ContentCategoryFk"`
+}
+
+type PhishingSimulationContentTemplateResult struct {
+	Subject string
+	Content string
+}
+
+type ScoredTemplate struct {
+	Score               float32
+	Template            *PhishingSimulationContentTemplate
+	RecognitionFeatures []PhishingSimulationRecognitionFeature
+}
+
+func (template *PhishingSimulationContentTemplate) GetScoredCombinations(vulnerabilities []PhishingSimulationUserVulnerability, recognitionFeatureDefinitions []PhishingSimulationRecognitionFeature) []ScoredTemplate {
+	var scoredCombinations []ScoredTemplate
+	recognitionFeatures, err := template.getApplicableRecognitionFeatures(recognitionFeatureDefinitions)
+	if err != nil {
+		slog.Error("Template Error", "error", err)
+		return scoredCombinations
+	}
+
+	combinations := utils.Combinations(recognitionFeatures.List(), 2)
+	for _, comb := range combinations {
+		var score float32 = 0
+		for _, feat := range comb {
+			vulnerability := utils.Find(
+				vulnerabilities,
+				func(vuln PhishingSimulationUserVulnerability) bool {
+					return *vuln.ContentCategory == *template.ContentCategory && *vuln.RecognitionFeature == feat
+				})
+			if vulnerability != nil {
+				score += vulnerability.Score
+			} else {
+				mostMatchingVulnerabilities := utils.FindAll(
+					vulnerabilities,
+					func(vuln PhishingSimulationUserVulnerability) bool {
+						return *vuln.RecognitionFeature == feat
+					})
+
+				var arithMean float32 = 0
+				for _, mostMatchingVuln := range mostMatchingVulnerabilities {
+					arithMean += mostMatchingVuln.Score
+				}
+				if len(mostMatchingVulnerabilities) > 0 {
+					arithMean = arithMean / float32(len(mostMatchingVulnerabilities))
+				}
+				if arithMean == 0 {
+					score += 1
+				} else {
+					score += arithMean
+				}
+			}
+		}
+
+		scoredCombination := ScoredTemplate{
+			Score:               score,
+			Template:            template,
+			RecognitionFeatures: comb,
+		}
+		scoredCombinations = append(scoredCombinations, scoredCombination)
+	}
+	return scoredCombinations
+}
+
+func (template *PhishingSimulationContentTemplate) getApplicableRecognitionFeatures(recognitionFeatureDefinitions []PhishingSimulationRecognitionFeature) (*utils.Set[PhishingSimulationRecognitionFeature], error) {
+	resultSet := utils.NewSet[PhishingSimulationRecognitionFeature]()
+
+	applicableInContent := extractRecognitionFeaturesUsed(template.Content)
+	applicableInSubject := extractRecognitionFeaturesUsed(template.Subject)
+	applicableTotal := applicableInContent.Union(&applicableInSubject)
+
+	for _, def := range recognitionFeatureDefinitions {
+		if def.IsAlwaysApplicable {
+			resultSet.Add(def)
+		}
+	}
+
+	for _, feat := range applicableTotal.List() {
+		defition := utils.Find(recognitionFeatureDefinitions, func(def PhishingSimulationRecognitionFeature) bool {
+			return def.Name == feat
+		})
+
+		if defition == nil {
+			err := errors.New("Undefined RecognitionFeature used in template")
+			return nil, err
+		}
+		resultSet.Add(*defition)
+	}
+	return resultSet, nil
+}
+
+func extractRecognitionFeaturesUsed(v string) utils.Set[string] {
+	var values utils.Set[string]
+
+	re := regexp.MustCompile(`\{RecognitionFeature\{([^}]+)\}\}`)
+	matches := re.FindAllStringSubmatch(v, -1)
+
+	for _, match := range matches {
+		if len(match) > 1 {
+			values.Add(match[1])
+		}
+	}
+	return values
 }
--- PhishingBackend/internal/domain_services/services/phishing_run_service.go
+++ PhishingBackend/internal/domain_services/services/phishing_run_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"log/slog"
 	"phishing_backend/internal/domain_model"
 	"phishing_backend/internal/domain_services/interfaces/email"
 	"phishing_backend/internal/domain_services/interfaces/repositories"
@@ -21,6 +22,9 @@ type PhishingRunServiceImpl struct {
 }
 
 func (s *PhishingRunServiceImpl) GenerateRun(user *domain_model.User) error {
+	scoredTemplates := s.getScoredTemplates(user.ID)
+	slog.Info("scored Templates", "info", scoredTemplates)
+
 	templates, err := s.PhishingSimulationRepository.GetTemplates()
 	if err != nil {
 		return err
@@ -32,31 +36,61 @@ func (s *PhishingRunServiceImpl) GenerateRun(user *domain_model.User) error {
 			ID:         uuid.New(),
 			UserFk:     user.ID,
 			TemplateFk: first.ID,
+			Subject:    first.Subject,
+			Content:    first.Content,
 		}
+
 		err = s.PhishingSimulationRepository.Create(&run)
 		if err != nil {
 			return err
 		}
 
-		email := domain_model.Email{
-			Content:   first.Content,
-			Recipient: user.Email,
-			Subject:   first.Subject,
-		}
-		err = s.EmailSender.Send(&email)
-		if err != nil {
-			return err
-		}
+		return nil //s.sendRun(&run)
+	}
+	return nil
+}
 
-		now := time.Now().UTC()
-		runPatch := domain_model.PhishingSimulationRunPatch{
-			ID:     run.ID,
-			SentAt: &now,
-		}
-		err = s.PhishingSimulationRepository.Update(&runPatch)
-		if err != nil {
-			return err
+func (s *PhishingRunServiceImpl) getScoredTemplates(userId uuid.UUID) []domain_model.ScoredTemplate {
+	var scoredTemplatesTotal []domain_model.ScoredTemplate
+	vulnerabilities, err := s.PhishingSimulationRepository.GetUserVulnerabilities(userId)
+	slog.Info("vulnerabilities", "info", vulnerabilities)
+	if err == nil {
+		recognitionFeatures, err := s.PhishingSimulationRepository.GetRecognitionFeatures()
+		if err == nil {
+			templates, err := s.PhishingSimulationRepository.GetTemplates()
+			slog.Info("templates", "info", templates)
+			if err == nil {
+				for _, template := range templates {
+					scoredTemplates := template.GetScoredCombinations(vulnerabilities, recognitionFeatures)
+					scoredTemplatesTotal = append(scoredTemplatesTotal, scoredTemplates...)
+				}
+			}
 		}
 	}
+	return scoredTemplatesTotal
+}
+
+func (s *PhishingRunServiceImpl) sendRun(run *domain_model.PhishingSimulationRun) error {
+	email := domain_model.Email{
+		Subject:   run.Subject,
+		Content:   run.Content,
+		Recipient: run.User.Email,
+	}
+
+	err := s.EmailSender.Send(&email)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now().UTC()
+	runPatch := domain_model.PhishingSimulationRunPatch{
+		ID:     run.ID,
+		SentAt: &now,
+	}
+	err = s.PhishingSimulationRepository.Update(&runPatch)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
