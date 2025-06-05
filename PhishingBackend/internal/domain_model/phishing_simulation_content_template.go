492f1b06710fab189571825cc352c63f9476546d
Mischa Binder <mischa.binder@stafag.ch>
2025-06-05 11:52:13 +0200

    Rewrite Logic to look for most suitable template with most suitable combination of recognition features (up to 2 per combination)

--- PhishingBackend/internal/domain_model/phishing_simulation_content_template.go
+++ PhishingBackend/internal/domain_model/phishing_simulation_content_template.go
@@ -3,6 +3,7 @@ package domain_model
 import (
 	"errors"
 	"log/slog"
+	"math"
 	"phishing_backend/internal/utils"
 	"regexp"
 
@@ -17,18 +18,16 @@ type PhishingSimulationContentTemplate struct {
 	ContentCategory   *PhishingSimulationContentCategory `gorm:"foreignKey:ContentCategoryFk"`
 }
 
-type PhishingSimulationContentTemplateResult struct {
-	Subject string
-	Content string
-}
-
 type ScoredTemplate struct {
-	Score               float32
-	Template            *PhishingSimulationContentTemplate
-	RecognitionFeatures []PhishingSimulationRecognitionFeature
+	Score                    float32
+	Template                 *PhishingSimulationContentTemplate
+	RecognitionFeatureValues []PhishingSimulationRecognitionFeatureValue
 }
 
-func (template *PhishingSimulationContentTemplate) GetScoredCombinations(vulnerabilities []PhishingSimulationUserVulnerability, recognitionFeatureDefinitions []PhishingSimulationRecognitionFeature) []ScoredTemplate {
+func (template *PhishingSimulationContentTemplate) GetScoredCombinations(
+	vulnerabilities []PhishingSimulationUserVulnerability,
+	recognitionFeatureDefinitions []PhishingSimulationRecognitionFeature,
+) []ScoredTemplate {
 	var scoredCombinations []ScoredTemplate
 	recognitionFeatures, err := template.getApplicableRecognitionFeatures(recognitionFeatureDefinitions)
 	if err != nil {
@@ -38,41 +37,62 @@ func (template *PhishingSimulationContentTemplate) GetScoredCombinations(vulnera
 
 	combinations := utils.Combinations(recognitionFeatures.List(), 2)
 	for _, comb := range combinations {
+		// Iterate over applicableRecognitionFeatures
+		// if in comb then select the respective level x > 1
+		// else select lowest level
+		var featureValuesComb []PhishingSimulationRecognitionFeatureValue
 		var score float32 = 0
-		for _, feat := range comb {
-			vulnerability := utils.Find(
-				vulnerabilities,
-				func(vuln PhishingSimulationUserVulnerability) bool {
-					return *vuln.ContentCategory == *template.ContentCategory && *vuln.RecognitionFeature == feat
-				})
-			if vulnerability != nil {
-				score += vulnerability.Score
-			} else {
-				mostMatchingVulnerabilities := utils.FindAll(
+		for _, feature := range recognitionFeatures.List() {
+			level := 0
+			if utils.Find(comb, func(f PhishingSimulationRecognitionFeature) bool {
+				return f.ID == feature.ID
+			}) != nil {
+				vulnerability := utils.Find(
 					vulnerabilities,
 					func(vuln PhishingSimulationUserVulnerability) bool {
-						return *vuln.RecognitionFeature == feat
+						return *vuln.ContentCategory == *template.ContentCategory && vuln.RecognitionFeature.ID == feature.ID
 					})
-
-				var arithMean float32 = 0
-				for _, mostMatchingVuln := range mostMatchingVulnerabilities {
-					arithMean += mostMatchingVuln.Score
-				}
-				if len(mostMatchingVulnerabilities) > 0 {
-					arithMean = arithMean / float32(len(mostMatchingVulnerabilities))
-				}
-				if arithMean == 0 {
-					score += 1
+				if vulnerability != nil {
+					score += vulnerability.Score
+					level = int(math.Round(float64(vulnerability.Score)))
 				} else {
-					score += arithMean
+					mostMatchingVulnerabilities := utils.FindAll(
+						vulnerabilities,
+						func(vuln PhishingSimulationUserVulnerability) bool {
+							return vuln.RecognitionFeature.ID == feature.ID
+						})
+
+					var arithMean float32 = 0
+					for _, mostMatchingVuln := range mostMatchingVulnerabilities {
+						arithMean += mostMatchingVuln.Score
+					}
+					if len(mostMatchingVulnerabilities) > 0 {
+						arithMean = arithMean / float32(len(mostMatchingVulnerabilities))
+					}
+					if arithMean == 0 {
+						score += 1
+					} else {
+						score += arithMean
+						level = int(math.Round(float64(arithMean)))
+					}
 				}
+			}
+
+			if feature.RecognitionFeatureValues != nil && len(*feature.RecognitionFeatureValues) > 0 {
+				mostSuitableValue := (*feature.RecognitionFeatureValues)[0]
+				for _, value := range (*feature.RecognitionFeatureValues)[1:] {
+					if math.Abs(float64(level-value.Level)) < math.Abs(float64(level-mostSuitableValue.Level)) {
+						mostSuitableValue = value
+					}
+				}
+				featureValuesComb = append(featureValuesComb, mostSuitableValue)
 			}
 		}
 
 		scoredCombination := ScoredTemplate{
-			Score:               score,
-			Template:            template,
-			RecognitionFeatures: comb,
+			Score:                    score,
+			Template:                 template,
+			RecognitionFeatureValues: featureValuesComb,
 		}
 		scoredCombinations = append(scoredCombinations, scoredCombination)
 	}
