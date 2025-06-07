ffffc4f03b3093a963931bce32c35c3bac3fafd5
Mischa Binder <mischa.binder@stafag.ch>
2025-06-07 23:13:31 +0200

    Add Phishing Simulation Controller

---
+++ PhishingBackend/internal/adapters/presentation/controllers/phishing_simulation_controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"net/http"
+	"phishing_backend/internal/adapters/presentation/api"
+	"phishing_backend/internal/adapters/presentation/error_handling"
+	"phishing_backend/internal/domain_model"
+	"phishing_backend/internal/domain_services/interfaces/repositories"
+	"phishing_backend/internal/domain_services/services"
+)
+
+type PhishingSimulationController struct {
+	PhishingRunService           services.PhishingRunService
+	PhishingSimulationRepository repositories.PhishingSimulationRepository
+}
+
+func (c *PhishingSimulationController) GetRun(w http.ResponseWriter, r *http.Request) {
+	runId, err := getPathVariable(r, "phishingSimulationRunId")
+	if err != nil {
+		error_handling.WriteErrorDetailResponse(w, err)
+		return
+	}
+	run, err := c.PhishingSimulationRepository.GetRun(runId)
+	if err != nil {
+		error_handling.WriteErrorDetailResponse(w, err)
+		return
+	}
+	runDto := toApiPhishingSimulationRun(run)
+	writeJsonResponse(w, http.StatusOK, &runDto)
+}
+
+func toApiPhishingSimulationRun(run *domain_model.PhishingSimulationRun) *api.PhishingSimulationRun {
+	dtoRun := api.PhishingSimulationRun{
+		PhishingSimulationRunId: (*run).ID,
+	}
+	dtoRecognitionFeatureValues := make([]api.PhishingSimulationRecognitionFeatureValue, len((*run).RecognitionFeatureValues))
+	for i, recognitionFeatureValue := range run.RecognitionFeatureValues {
+		dtoRecognitionFeatureValue := api.PhishingSimulationRecognitionFeatureValue{
+			RecognitionFeatureValueId: recognitionFeatureValue.ID,
+		}
+		dtoRecognitionFeatureValues[i] = dtoRecognitionFeatureValue
+	}
+	dtoRun.RecognitionFeatureValues = dtoRecognitionFeatureValues
+	return &dtoRun
+}
