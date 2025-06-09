13dbcb95413721d2c326b55686d3d16f70e0f58b
Mischa Binder <mischa.binder@stafag.ch>
2025-06-09 14:58:24 +0200

    Fix nil pointer dereference

--- PhishingBackend/internal/domain_services/services/phishing_orchestrator.go
+++ PhishingBackend/internal/domain_services/services/phishing_orchestrator.go
@@ -21,7 +21,9 @@ type PhishingOrchestratorImpl struct {
 }
 
 func (p *PhishingOrchestratorImpl) StartPhishingRunGenerationJob() {
-	go StartRandomCronJob(15*time.Minute, 60*time.Minute, p.generatePhishingRuns)
+	// go StartRandomCronJob(15*time.Minute, 60*time.Minute, p.generatePhishingRuns)
+	p.generatePhishingRuns(time.Now().UTC())
+	go StartRandomCronJob(0*time.Minute, 2*time.Minute, p.generatePhishingRuns)
 }
 
 func (p *PhishingOrchestratorImpl) StartPhishingRunStregthDetectionJob() {}
@@ -45,6 +47,7 @@ func (p *PhishingOrchestratorImpl) generatePhishingRuns(currentTime time.Time) {
 		}
 
 		if latestRun == nil ||
+			latestRun.Email == nil ||
 			latestRun.Email.SentAt == nil ||
 			latestRun.Email.SentAt.Add(periodUntilNextRun).UTC().Before(currentTime) {
 			err = p.PhishingRunService.GenerateRun(&user)
