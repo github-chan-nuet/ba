9227faa93ff4a23acaf109c3d36d64dc2516ee18
Mischa Binder <mischa.binder@stafag.ch>
2025-06-10 11:37:22 +0200

    Add separate component CallToAction

--- PhishingEducator/app/routes/(Marketing)/index.tsx
+++ PhishingEducator/app/routes/(Marketing)/index.tsx
@@ -1,11 +1,7 @@
-import {Button, tokens} from "@fluentui/react-components";
-import MarketingStyles from "@styles/Marketing.module.scss";
-import lines from '@assets/images/lines.svg';
-import { ShieldTask28Filled } from "@fluentui/react-icons";
 import Hero from "@components/(Marketing)/Hero";
 import FeatureGrid from "@components/(Marketing)/FeatureGrid";
-import { useOutletContext } from "react-router";
 import Stats from "@components/(Marketing)/Stats";
+import CallToAction from "@components/(Marketing)/CallToAction";
 
 export function meta() {
     return [
@@ -21,10 +17,7 @@ export function meta() {
     ]
 }
 
-type HomeContext = {setAuthOpen: () => void};
 export default function Home() {
-    const context = useOutletContext<HomeContext>();
-
     return (
         <>
             <Hero
@@ -32,41 +25,9 @@ export default function Home() {
                 subtitle={<>Scrolle nach unten und erfahre mehr darüber, wie du dich effektiv vor Phishing schützen kannst!</>}
                 showScrollIcon={true}
             />
-
             <FeatureGrid />
-
             <Stats />
-
-            <section className={MarketingStyles.Section + " " + MarketingStyles.SecurawareSolution}>
-                <div style={{
-                    backgroundColor: tokens.colorPaletteBlueBorderActive,
-                    backgroundImage: "url(\"" + lines + "\")",
-                    backgroundRepeat: "no-repeat",
-                    height: "100%",
-                    borderRadius: "2rem",
-                }}/>
-                <div style={{
-                    textAlign: "center",
-                    display: "flex",
-                    justifyContent: "space-around",
-                    flexDirection: "column",
-                }}>
-                    <h3 className={MarketingStyles.Section__title}>Securaware als Lösung</h3>
-                    <p className={MarketingStyles.Section__text}>Wie schütze ich mich vor Phishing-Angriffen? Wie identifiziere ich diese? Securaware schützt dich und hilft dir.</p>
-                    <div>
-                        <Button
-                            size="large"
-                            shape={"circular"}
-                            iconPosition="after"
-                            appearance="primary"
-                            icon={<ShieldTask28Filled/>}
-                            style={{width: "15rem", height: "4rem"}}
-                            onClick={context.setAuthOpen}>
-                            Schütze dich jetzt
-                        </Button>
-                    </div>
-                </div>
-            </section>
+            <CallToAction />
         </>
     );
 }
