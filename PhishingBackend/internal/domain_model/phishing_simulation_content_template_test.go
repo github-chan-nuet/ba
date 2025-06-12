491cdeb1c892bedf817b84cb7b7274d090baeb9a
Patrick.Scheidegger <patrick.scheidegger@ost.ch>
2025-06-12 19:11:03 +0200

    add tests

--- PhishingBackend/internal/domain_model/phishing_simulation_content_template_test.go
+++ PhishingBackend/internal/domain_model/phishing_simulation_content_template_test.go
@@ -1,6 +1,7 @@
 package domain_model
 
 import (
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -74,4 +75,198 @@ func TestFindAllNoMatch(t *testing.T) {
 
 	// then
 	assert.Empty(t, result)
+}
+
+func TestGetApplicableRecognitionFeatures_Success(t *testing.T) {
+	// given
+	template := PhishingSimulationContentTemplate{
+		Subject: "{RecognitionFeature{Name1}}",
+		Content: "{RecognitionFeature{Name2}}",
+	}
+	def1 := PhishingSimulationRecognitionFeature{Name: "Name1", IsAlwaysApplicable: false}
+	def2 := PhishingSimulationRecognitionFeature{Name: "Name2", IsAlwaysApplicable: false}
+	def3 := PhishingSimulationRecognitionFeature{Name: "Always", IsAlwaysApplicable: true}
+
+	// when
+	features, err := template.getApplicableRecognitionFeatures([]PhishingSimulationRecognitionFeature{def1, def2, def3})
+
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(features.List()))
+}
+
+func TestGetApplicableRecognitionFeatures_UndefinedFeature(t *testing.T) {
+	// given
+	template := PhishingSimulationContentTemplate{
+		Content: "{RecognitionFeature{Unknown}}",
+	}
+
+	// when
+	features, err := template.getApplicableRecognitionFeatures([]PhishingSimulationRecognitionFeature{})
+
+	// then
+	assert.Nil(t, features)
+	assert.Error(t, err)
+	assert.Equal(t, "Undefined RecognitionFeature used in template", err.Error())
+}
+
+func TestGetScoredCombinations_Basic(t *testing.T) {
+	// given
+	categoryID := uuid.New()
+	featureID := uuid.New()
+	template := &PhishingSimulationContentTemplate{
+		Subject:           "{RecognitionFeature{Name1}}",
+		ContentCategoryFk: categoryID,
+		ContentCategory:   &PhishingSimulationContentCategory{ID: categoryID},
+	}
+	value := PhishingSimulationRecognitionFeatureValue{
+		Level: 2,
+	}
+	feature := PhishingSimulationRecognitionFeature{
+		ID:                       featureID,
+		Name:                     "Name1",
+		RecognitionFeatureValues: &[]PhishingSimulationRecognitionFeatureValue{value},
+	}
+	vuln := PhishingSimulationUserVulnerability{
+		ContentCategory:    &PhishingSimulationContentCategory{ID: categoryID},
+		RecognitionFeature: &feature,
+		Score:              2.5,
+	}
+
+	// when
+	result := template.GetScoredCombinations([]PhishingSimulationUserVulnerability{vuln}, []PhishingSimulationRecognitionFeature{feature})
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.InDelta(t, 2.5, result[0].Score, 0.01)
+	assert.Equal(t, template, result[0].Template)
+	assert.Equal(t, 1, len(result[0].RecognitionFeatureValues))
+}
+
+func TestGetApplicableRecognitionFeatures_OnlyAlwaysApplicable(t *testing.T) {
+	// given
+	template := PhishingSimulationContentTemplate{
+		Content: "No feature used",
+		Subject: "Still no feature",
+	}
+	def := PhishingSimulationRecognitionFeature{Name: "Always", IsAlwaysApplicable: true}
+
+	// when
+	result, err := template.getApplicableRecognitionFeatures([]PhishingSimulationRecognitionFeature{def})
+
+	// then
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []PhishingSimulationRecognitionFeature{def}, result.List())
+}
+
+func TestGetApplicableRecognitionFeatures_DuplicateUsage(t *testing.T) {
+	// given
+	template := PhishingSimulationContentTemplate{
+		Content: "{RecognitionFeature{Feat}} and again {RecognitionFeature{Feat}}",
+	}
+	def := PhishingSimulationRecognitionFeature{Name: "Feat", IsAlwaysApplicable: false}
+
+	// when
+	result, err := template.getApplicableRecognitionFeatures([]PhishingSimulationRecognitionFeature{def})
+
+	// then
+	assert.NoError(t, err)
+	assert.Len(t, result.List(), 1)
+}
+
+func TestGetScoredCombinations_NoMatchingVulnerability(t *testing.T) {
+	// given
+	categoryID := uuid.New()
+	featureID := uuid.New()
+	template := &PhishingSimulationContentTemplate{
+		Subject:           "{RecognitionFeature{Feat}}",
+		ContentCategoryFk: categoryID,
+		ContentCategory:   &PhishingSimulationContentCategory{ID: categoryID},
+	}
+	featureValue := PhishingSimulationRecognitionFeatureValue{Level: 1}
+	feature := PhishingSimulationRecognitionFeature{
+		ID:                       featureID,
+		Name:                     "Feat",
+		RecognitionFeatureValues: &[]PhishingSimulationRecognitionFeatureValue{featureValue},
+	}
+	vuln := PhishingSimulationUserVulnerability{
+		RecognitionFeature: &PhishingSimulationRecognitionFeature{
+			ID:   uuid.New(),
+			Name: "Unrelated",
+		},
+		ContentCategory: &PhishingSimulationContentCategory{ID: categoryID},
+		Score:           4,
+	}
+
+	// when
+	result := template.GetScoredCombinations([]PhishingSimulationUserVulnerability{vuln}, []PhishingSimulationRecognitionFeature{feature})
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.Equal(t, float32(1), result[0].Score)
+}
+
+func TestGetScoredCombinations_ArithmeticMeanFallback(t *testing.T) {
+	// given
+	categoryID1 := uuid.New()
+	categoryID2 := uuid.New()
+	featureID := uuid.New()
+	template := &PhishingSimulationContentTemplate{
+		Subject:           "{RecognitionFeature{Feat}}",
+		ContentCategoryFk: categoryID1,
+		ContentCategory:   &PhishingSimulationContentCategory{ID: categoryID1},
+	}
+	featureValue := PhishingSimulationRecognitionFeatureValue{Level: 2}
+	feature := PhishingSimulationRecognitionFeature{
+		ID:                       featureID,
+		Name:                     "Feat",
+		RecognitionFeatureValues: &[]PhishingSimulationRecognitionFeatureValue{featureValue},
+	}
+	vuln1 := PhishingSimulationUserVulnerability{
+		RecognitionFeature: &feature,
+		ContentCategory:    &PhishingSimulationContentCategory{ID: categoryID2}, // different category
+		Score:              2,
+	}
+	vuln2 := PhishingSimulationUserVulnerability{
+		RecognitionFeature: &feature,
+		ContentCategory:    &PhishingSimulationContentCategory{ID: categoryID2},
+		Score:              4,
+	}
+
+	// when
+	result := template.GetScoredCombinations([]PhishingSimulationUserVulnerability{vuln1, vuln2}, []PhishingSimulationRecognitionFeature{feature})
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.InDelta(t, 3.0, result[0].Score, 0.01) // mean of 2 and 4
+}
+
+func TestGetScoredCombinations_LevelCorrectedToMin1(t *testing.T) {
+	// given
+	categoryID := uuid.New()
+	featureID := uuid.New()
+	template := &PhishingSimulationContentTemplate{
+		Subject:           "{RecognitionFeature{Feat}}",
+		ContentCategoryFk: categoryID,
+		ContentCategory:   &PhishingSimulationContentCategory{ID: categoryID},
+	}
+	featureValue1 := PhishingSimulationRecognitionFeatureValue{Level: 0}
+	featureValue2 := PhishingSimulationRecognitionFeatureValue{Level: 1}
+	feature := PhishingSimulationRecognitionFeature{
+		ID:                       featureID,
+		Name:                     "Feat",
+		RecognitionFeatureValues: &[]PhishingSimulationRecognitionFeatureValue{featureValue1, featureValue2},
+	}
+	vuln := PhishingSimulationUserVulnerability{
+		RecognitionFeature: &feature,
+		ContentCategory:    &PhishingSimulationContentCategory{ID: categoryID},
+		Score:              0, // level = 0, but should be corrected
+	}
+
+	// when
+	result := template.GetScoredCombinations([]PhishingSimulationUserVulnerability{vuln}, []PhishingSimulationRecognitionFeature{feature})
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.GreaterOrEqual(t, (result[0].RecognitionFeatureValues)[0].Level, 1)
 }
