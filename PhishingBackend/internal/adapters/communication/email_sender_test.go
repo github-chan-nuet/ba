0115d8a8b96be636c9ef277c6ebf50a36d4c4d45
Mischa Binder <mischa.binder@stafag.ch>
2025-06-29 21:01:45 +0200

    Fix tests

--- PhishingBackend/internal/adapters/communication/email_sender_test.go
+++ PhishingBackend/internal/adapters/communication/email_sender_test.go
@@ -2,13 +2,15 @@ package communication
 
 import (
 	"bytes"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
 	"mime"
 	"net/smtp"
 	"phishing_backend/internal/domain_model"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 type sendMailParams struct {
@@ -27,6 +29,8 @@ func createEmailSenderImpl() *EmailSenderImpl {
 		SmtpPw:   "pw",
 		SmtpAddr: "addr",
 		SmtpHost: "host",
+		NowFn:    getFixedDate,
+		MsgIDFn:  getFixedMessageId,
 		SendMailFn: func(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
 			sendMailArgs = sendMailParams{
 				addr: addr,
@@ -40,12 +44,22 @@ func createEmailSenderImpl() *EmailSenderImpl {
 	}
 }
 
+func getFixedDate() time.Time {
+	return time.Date(2025, 5, 12, 13, 45, 23, 0, time.UTC)
+}
+
+func getFixedMessageId() string {
+	return "<fixed-message-id@domain>"
+}
+
 func getWantMessage(email *domain_model.Email, content string) []byte {
 	var msg bytes.Buffer
 	msg.WriteString("From: " + email.Sender + newLine)
 	msg.WriteString("To: " + email.Recipient + newLine)
 	encodedSubject := mime.QEncoding.Encode("utf-8", email.Subject)
 	msg.WriteString("Subject: " + encodedSubject + newLine)
+	msg.WriteString("Date: " + getFixedDate().Format(time.RFC1123Z) + newLine)
+	msg.WriteString("Message-ID: " + getFixedMessageId() + newLine)
 	msg.WriteString("MIME-Version: 1.0" + newLine)
 	msg.WriteString("Content-Type: text/html; charset=utf-8" + newLine)
 	msg.WriteString("Content-Transfer-Encoding: quoted-printable" + newLine)
